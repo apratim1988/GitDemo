6fcc188edf9962a27b05e51bb34bc840c5000585
apratim <apratim.chaudhuri@gmail.com>
2021-06-01 01:47:48 -0700

    first commit

---
+++ test_homepage.py
@@ -0,0 +1,26 @@
+import pytest
+from selenium import webdriver
+
+from ExcelDemoPackage.HomePageData import HomePageData
+from pageObject.HomePage import HomePage
+from utilities.BaseClass import BaseClass
+
+
+class TestHomePage(BaseClass):
+
+    def test_formsubmission(self):
+        log = self.getlogger() #accessing the logger object
+        log.info("msg")
+        homePage = HomePage(self.driver)
+        homePage.getname().send_keys("testname")
+        homePage.getemail().send_keys("test@test.com")
+        homePage.getcheckbox().click
+        homePage.getsubmit().click
+        self.driver.refresh()
+
+    @pytest.fixture(params=HomePageData.getTestData("testCase2"))
+    def getData(self,request):
+        return request.param
+
+
+
